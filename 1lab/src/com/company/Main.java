47ddd1192d10f6c57298357e2a16d051a38eb8ad
danil512 <titov.d.r@edu.mirea.ru>
2019-09-28 09:18:05 +0300

    Update Main.java

--- 1lab/src/com/company/Main.java
+++ 1lab/src/com/company/Main.java
@@ -7,10 +7,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Введите количество элементов массива");
         Scanner in = new Scanner(System.in);
-        int k = in.nextInt();
-        mass m = new mass(k);
-	    int a =0;
-	    while (a!=9){
+        int n = in.nextInt();
+        mass m = new mass(n);
+	    int answer =0;
+	    while (answer!=9){
 
             System.out.println("1 - ввести вручную");
             System.out.println("2 - автозаполнение");
@@ -18,27 +18,29 @@ public class Main {
             System.out.println("4 - вывод л-п");
             System.out.println("5 - func1");
             System.out.println("6 - func2");
-            a = in.nextInt();
-            if (a==1){
+            System.out.println("9 - exit");
+            answer = in.nextInt();
+            if (answer==1){
                 m.put(in);
             }
-            if (a==2){
+            if (answer==2){
                 m.ran();
             }
-            if (a==3){
+            if (answer==3){
                 m.showr();
             }
-            if (a==4){
+            if (answer==4){
                 m.showl();
             }
-            if (a==5){
-                int kl = in.nextInt();
-                System.out.println("Введите число для сравнения.");
-                m.opr(kl);
-            }
-            if (a==6){
+            if (answer==5){
 
-                m.opr2();
+                System.out.println("Введите число для сравнения.");
+                int num = in.nextInt();
+                m.opr(num);
+            }
+            if (answer==6){
+
+               m = m.opr2();
             }
     }
     }
