8170db3727e4a1bbd4acc76720eccffdd49bd01a
danil512 <titov.d.r@edu.mirea.ru>
2019-10-30 10:43:03 +0300

    Create admin.java

---
+++ 3 лаба/src/com/company/admin.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class admin extends user{
+
+
+    public admin(String Name, String Login, String Password) {
+        super(Name, Login, Password);
+        option =2;
+    }
+
+}
+
