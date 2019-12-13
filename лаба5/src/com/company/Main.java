7dd5377ccfaef5b325caa894c39f57db27091beb
danil512 <titov.d.r@edu.mirea.ru>
2019-12-13 14:49:45 +0300

    Create Main.java

---
+++ лаба5/src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+public class Main {
+
+
+    public static void main(String[] args) {
+        LogisticSystem st = new LogisticSystem();
+        st.adduser("ad","ad","ad","ad",2);
+        st.switchbtw(false,false,false);
+
+    }
+}
