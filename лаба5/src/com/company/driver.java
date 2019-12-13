f55e89b8215503f217a6d3397ac36fa5c89aa979
danil512 <titov.d.r@edu.mirea.ru>
2019-12-13 14:49:35 +0300

    Create driver.java

---
+++ лаба5/src/com/company/driver.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class driver extends user {
+
+
+    public driver(String Name, String Login, String Password) {
+        super(Name, Login, Password);
+        option = 1;
+    }
+}
